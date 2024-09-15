1e57617953d71c1859c130c50f85219c230fea8b
Lobna Allam <allamlobna@gmail.com>
2024-09-15 16:41:52 -0400

    clean up

--- allamlobna_week_2.py
+++ allamlobna_week_2.py
@@ -1,9 +1,9 @@
 '''2. Uses conditionals (if-then-else) and exception handling.'''
 
 try:
-    value = int(input("Provide an even number:"))
+    value = int(input('Provide an even number:'))
 except ValueError:
-    print("The input provided is not a number.")
+    print('The input provided is not a number.')
 else:
     if value%2 == 0:
         print('The number is even.')
